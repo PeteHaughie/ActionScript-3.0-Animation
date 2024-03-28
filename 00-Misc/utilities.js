7ebc787f995b17687be6ae4acfb75d4592506fc9
Pete Haughie <pete@petehaughie.com>
2024-03-28 17:00:42 +0100

    Moved the canvas creator into the utilities module as it's going to get used a lot

--- 00-Misc/utilities.js
+++ 00-Misc/utilities.js
@@ -1,9 +1,11 @@
-import pythagoras from "./pythagoras";
+import CanvasCreator from "./canvas";
 import deg2rad from "./deg2rad";
+import pythagoras from "./pythagoras";
 import rad2deg from "./rad2deg";
 
 export {
-  pythagoras,
+  CanvasCreator,
   deg2rad,
+  pythagoras,
   rad2deg
 }
